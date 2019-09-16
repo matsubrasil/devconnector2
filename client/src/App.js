4debd874193d6d06d22c0d5214e122230e9571a4
matsubrasil <matsuboy@outlook.com>
2019-09-16 17:15:38 -0300

    add Landing page & routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
 const App = () => (
-  <>
-    <h1>App</h1>
-  </>
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </>
+  </Router>
 );
 
 export default App;
